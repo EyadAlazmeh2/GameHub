f1c18fd347ce9eb14af03ac8f4841bab77267924
EyadAlazmeh2 <eyadoalazmeh@gmail.com>
2023-11-06 17:37:14 +0300

    Fix the Navbar

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -2,16 +2,19 @@ import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
 import { useRef } from "react";
 import { BsSearch } from "react-icons/bs";
 import useGameQuery from "../store";
+import { useNavigate } from "react-router-dom";
 
 const SearchInput = () => {
   const {gameQuery, add} = useGameQuery()
   const ref = useRef<HTMLInputElement>(null);
+  const navigate = useNavigate()
   return (
     <>
       <form
         onSubmit={(event) => {
           event.preventDefault();
           if (ref.current) add({searchText: ref.current.value});
+          navigate('/')
         }}
       >
         <InputGroup>
